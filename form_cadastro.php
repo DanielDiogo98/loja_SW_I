dd811bceee8323a95393b81ac83b479cc13478a0
Daniel DIogo <159474740+DanielDiogo98@users.noreply.github.com>
2025-08-20 12:25:37 -0300

    Add files via upload

---
+++ form_cadastro.php
@@ -0,0 +1,23 @@
+<?php
+include 'pedaco.php';
+?>
+<div class="container">
+    <h1>Formulario para cadastro de produto</h1>
+    <form action="inserir.php" method="POST">
+        <div class="mb-3">
+            <input type="text" class="form-control" placeholder="insira seu nome" name="nome">
+            
+        </div>
+        <div class="mb-3">
+            <input type="text" class="form-control" placeholder="Digite o preço" name="preco">
+            
+        </div>
+        <div class="mb-3">
+            <input type="text" class="form-control" placeholder="Digite a quantidade" name="quantidade">
+            
+        </div>
+
+
+        <button type="submit" class="btn btn-primary">enviar</button>
+    </form>
+</div>
---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+    include 'pedaco.php';
+?>
+    <div class="container">
+        <div class="btn-group" role="group" aria-label="Basic mixed styles example">
+            <a href="form_cadastro.php" button" class="btn btn-danger">cadastrar produto</button>
+            <a href="listar.php" type="button" class="btn btn-warning">listar produto</button>
+        </div>
+    </div>
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js" integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q" crossorigin="anonymous"></script>
+</body>
+
+</html>
